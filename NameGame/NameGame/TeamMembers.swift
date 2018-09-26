c2807561515857a04e5f54d9fa7cba005e4277a6
TJ Barber <me@tjbarber.org>
2018-09-26 12:46:16 -0600

    The TeamMember data structure outgrew a struct once I needed to use a UIImage on Headshot. Moved them over to classes.

--- NameGame/NameGame/TeamMembers.swift
+++ NameGame/NameGame/TeamMembers.swift
@@ -6,12 +6,12 @@
 //  Copyright © 2018 WillowTree Apps. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 typealias TeamMembers = [TeamMember]
 typealias TeamMembersDict = [String: TeamMember]
 
-struct TeamMember: Codable {
+class TeamMember: Codable {
     let id: String
     let type: String = "people"
     let slug: String
@@ -20,15 +20,40 @@ struct TeamMember: Codable {
     let headshot: Headshot
     let socialLinks: [SocialLink]
     let bio: String?
+    
+    init(id: String, slug: String, jobTitle: String?, firstName: String, lastName: String, headshot: Headshot, socialLinks: [SocialLink], bio: String?) {
+        self.id = id
+        self.slug = slug
+        self.jobTitle = jobTitle
+        self.firstName = firstName
+        self.lastName = lastName
+        self.headshot = headshot
+        self.socialLinks = socialLinks
+        self.bio = bio
+    }
 }
 
-struct Headshot: Codable {
+class Headshot: Codable {
     let type: String = "image"
     let mimeType: MIMEType?
     let id: String
     let url: String?
     let alt: String
     let height, width: Int?
+    var image: UIImage?
+    
+    private enum CodingKeys: String, CodingKey {
+        case type, mimeType, id, url, alt, height, width
+    }
+    
+    init(mimeType: MIMEType?, id: String, url: String?, alt: String, height: Int?, width: Int?) {
+        self.mimeType = mimeType
+        self.id = id
+        self.url = url
+        self.alt = alt
+        self.height = height
+        self.width = width
+    }
 }
 
 enum MIMEType: String, Codable {
