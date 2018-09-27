34ebe6db530fd1d4b1f47ce0be9a87d583d7f6f0
TJ Barber <me@tjbarber.org>
2018-09-26 18:02:57 -0600

    Created a time formatting helper that uses DateComponentsFormatter instead of building my own method

---
+++ NameGame/NameGame/Helpers/TimeDisplayHelper.swift
@@ -0,0 +1,28 @@
+//
+//  TimeDisplayHelper.swift
+//  NameGame
+//
+//  Created by TJ Barber on 9/26/18.
+//  Copyright © 2018 WillowTree Apps. All rights reserved.
+//
+
+import Foundation
+
+class TimeFormattingHelper {
+    let formatter = DateComponentsFormatter()
+    
+    init() {
+        formatter.allowedUnits = [.minute, .second]
+        formatter.unitsStyle = .positional
+        formatter.zeroFormattingBehavior = .pad
+    }
+    
+    func timeStringFromInterval(_ interval: Int) -> String {
+        guard let timeInterval = TimeInterval(exactly: interval),
+              let formattedString = formatter.string(from: timeInterval) else {
+            return "0:00"
+        }
+        
+        return formattedString
+    }
+}
